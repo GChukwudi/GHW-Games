9fd3402164c805fc188e5ccaac47d19464b97657
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-12 17:09:37 +0200

    updated

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import cv2
-import cvzone
+from cvzone.HandTrackingModule import HandDetector
+
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 650)
-cap.set(4, 480)
+cap.set(3, 350)
+cap.set(4, 280)
 
+detector = HandDetector(detectionCon=1)
 
 while True:
     imBg = cv2.imread("resources/bg.png")
     success, img = cap.read()
 
-    imgScaled = cv2.resize(img, (0, 0), None, 0.5, 0.5)
-    imgScaled = imgScaled[:, 80:480]
+    imgScaled = cv2.resize(img, (280, 280))
 
-    imBg[233:473, 560:795] = imgScaled
+    row_offset = 80
+    col_offset = 80
+
+    start_row = 233 - row_offset
+    end_row = start_row + imgScaled.shape[0]
+    start_col = 560 - col_offset
+    end_col = start_col + imgScaled.shape[1]
+
+    # Find the hand and its landmarks
+    hands, img = detector.findHands(imgScaled)
+
+    imBg[start_row:end_row, start_col:end_col] = imgScaled
 
     cv2.imshow("Image", img)
     cv2.imshow("Background", imBg)
